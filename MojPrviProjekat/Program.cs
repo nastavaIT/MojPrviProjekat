b3fce689848cc19075590b66b683fcd43fb60134
vesnastanojevic <stanojevicvesna03@gmail.com>
2025-11-10 11:03:01 +0100

    Add project files.

---
+++ MojPrviProjekat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MojPrviProjekat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zdravo iz mog prvog C# projekta na GitHub-u!");
+            Console.WriteLine("Autor: nastavaIT");
+            Console.WriteLine("Datum: " + DateTime.Now.ToString("dd.MM.yyyy. HH:mm"));
+            Console.ReadLine();
+
+        }
+    }
+}
